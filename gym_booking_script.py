be3cb0fbd72e9455ac63fb019cffc9ed789d8450
Thomas Farneti <thomas.farneti@datalogic.com>
2024-01-09 20:26:52 +0100

    Small improvements removed yaml config added env variables removed structured logger

--- gym_booking_script.py
+++ gym_booking_script.py
@@ -1,70 +1,30 @@
-import os
 import requests
 import structlog
 import logging
 from datetime import datetime, timedelta
 from time import sleep
 import argparse
-import yaml
 
 def configure_logging(log_level=logging.INFO, log_file=None):
     # Configure the standard logging module for controlling the log level
-    logging.basicConfig(level=log_level)
-
-    # Configure structlog for more structured logging
-    structlog.configure(
-        processors=[
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            structlog.processors.UnicodeDecoder(),
-            structlog.processors.ExceptionPrettyPrinter(),
-            structlog.processors.JSONRenderer(),
-        ],
-        context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
-        cache_logger_on_first_use=True,
-    )
+    logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # If log_file is provided, configure a FileHandler to write logs to the specified file
     if log_file:
-        file_handler = logging.FileHandler(log_file)
+        file_handler = logging.FileHandler(log_file, mode='a')  # Use 'a' for append mode
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
         logging.getLogger().addHandler(file_handler)
 
-def read_config(log, config_file_path='config.yaml'):
-    try:
-        with open(config_file_path, 'r') as config_file:
-            config = yaml.safe_load(config_file)
-            if not config:
-                raise ValueError("Config file is empty or not in YAML format.")
-            
-            # Load information from environment variables
-            config['API']['booking_url'] = os.environ.get('API_URL', config['API']['booking_url'])
-            config['API']['login_url'] = os.environ.get('API_LOGIN_URL', config['API']['login_url'])
-            
-        return config
-    except FileNotFoundError:
-        log.error(f"Error: Config file '{config_file_path}' not found.")
-    except yaml.YAMLError as e:
-        log.error(f"Error parsing '{config_file_path}': {e}")
-    except Exception as e:
-        log.error(f"An unexpected error occurred while reading the configuration: {e}")
-
-    return None
-
-def get_session_id(api_url, log):
+def get_session_id(log, email, psw):
     payload = {
         'versione': '26',
         'tipo': 'web',
-        'pass': os.environ.get('API_PASSWORD', ''),
-        'mail': os.environ.get('API_EMAIL', ''),
+        'pass': psw,
+        'mail': email
     }
 
-    response = send_request(api_url, payload, log)
+    response = send_request('https://app.shaggyowl.com/funzioniapp/v407/loginApp', payload, log)
 
     if isinstance(response, dict):
         session_info = response.get('parametri', {}).get('sessione', {})
@@ -82,9 +42,11 @@ def send_request(api_url, payload, log):
     attempt_counter = 0
     retry_interval = 300  # Retry interval in seconds, starting with 5 minutes
 
+    headers =  {'content-type':'application/x-www-form-urlencoded; charset=utf-8'}
+
     while attempt_counter < max_attempts:
         try:
-            response = requests.post(api_url, data=payload, timeout=(5, 30))
+            response = requests.post(api_url, data=payload,headers=headers, timeout=(5, 30))
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
@@ -104,14 +66,14 @@ def is_already_booked(response):
 def is_successful_booking(response):
     return response and response.get('status') == 2
 
-def get_schedule_id_for_7am(api_url, session_id, target_date, log):
+def get_schedule_id_for_7am(session_id, target_date, log):
     data = {
         'id_sede': '5194',
         'codice_sessione': session_id,
         'giorno': target_date,
     }
 
-    response = send_request(api_url, data, log)
+    response = send_request('https://app.shaggyowl.com/funzioniapp/v407/palinsesti', data, log)
 
     if response:
         lista_risultati = response.get('parametri', {}).get('lista_risultati', [])
@@ -129,21 +91,21 @@ def get_schedule_id_for_7am(api_url, session_id, target_date, log):
     log.warning("No schedule found for 7 am on the specified date.")
     return None
 
-def book_gym_lesson(api_url, session_id, schedule_date, schedule_id, log):
+def book_gym_lesson(session_id, schedule_date, schedule_id, log):
     data = {
         'id_sede': '5194',
         'codice_sessione': session_id,
         'id_orario_palinsesto': schedule_id,
         'data': schedule_date,
     }
-    return send_request(api_url, data, log)
+    return send_request('https://app.shaggyowl.com/funzioniapp/v407/prenotazione_new', data, log)
 
-def logout(api_url, session_id, log):
+def logout(session_id, log):
     data = {
         'codice_sessione': session_id,
     }
 
-    response = send_request(api_url, data, log)
+    response = send_request('https://app.shaggyowl.com/funzioniapp/v407/logout', data, log)
 
     if response and response.get('status') == 2:
         log.info("Successfully logged out.")
@@ -152,29 +114,25 @@ def logout(api_url, session_id, log):
 
 def main():
     parser = argparse.ArgumentParser(description="Automated Gym Booking Script")
-    parser.add_argument("--config", default="config.yaml", help="Path to the configuration file (default: config.yaml)")
+    parser.add_argument("--email", required=True, help="Your email for login")
+    parser.add_argument("--password", required=True, help="Your password for login")
+    parser.add_argument("--log-level", default="INFO", help="Log level (default: INFO)")
+    parser.add_argument("--log-file", help="Path to the log file")
+
     args = parser.parse_args()
 
-    # Read log configuration from environment variables or use defaults
-    log_level = getattr(logging, os.environ.get('LOG_LEVEL', 'INFO').upper(), logging.INFO)
-    log_file = os.environ.get('LOG_FILE')
+    # Read log configuration from command-line arguments
+    log_level = getattr(logging, args.log_level.upper(), logging.INFO)
+    log_file = args.log_file
 
     configure_logging(log_level=log_level, log_file=log_file)
-    log = structlog.get_logger()
+    log = logging.getLogger(__name__)
 
-    config = read_config(log)
-
-    if config is None:
-        log.error(f"Unable to load configuration. Please check the configuration file '{args.config}' and try again.")
-        return
-
-    api_url = config['API']['booking_url']
-    login_url = config['API']['login_url']
-    logout_url = config['API']['logout_url']
-    schedule_api_url = config['API']['schedule_url']
+    email = args.email
+    psw = args.password
 
     # Get session ID from the login API
-    session_id = get_session_id(login_url, log)
+    session_id = get_session_id(log, email=email, psw=psw)
     if session_id is None:
         log.error("Failed to obtain the session ID. Exiting.")
         return
@@ -183,37 +141,28 @@ def main():
     book_date = today + timedelta(days=4)
     schedule_date = book_date.strftime('%Y-%m-%d')
 
-    schedule_id = get_schedule_id_for_7am(schedule_api_url, session_id, schedule_date, log)
+    schedule_id = get_schedule_id_for_7am(session_id, schedule_date, log)
 
     if schedule_id is not None:
         log.info("Attempting to book a gym lesson", date=book_date, scheduled_time='07:00')
         log.debug("API Request Payload", payload={'id_sede': '5194', 'codice_sessione': session_id, 'id_orario_palinsesto': schedule_id, 'data': schedule_date})
 
-        attempt_counter = 0
-        max_attempts = 3  # You can adjust the number of retry attempts
+        response = book_gym_lesson(session_id, schedule_date, schedule_id, log)
 
-        while attempt_counter < max_attempts:
-            response = book_gym_lesson(api_url, session_id, schedule_date, schedule_id, log)
+        if response is not None:
+            log.debug("API Response Payload", payload=response)
 
-            if response is not None:
-                log.debug("API Response Payload", payload=response)
+            if is_already_booked(response):
+                log.warning("You are already booked for the selected time.")
+            elif is_successful_booking(response):
+                log.info("Congratulations! You have successfully booked a gym lesson.")
 
-                if is_already_booked(response):
-                    log.warning("You are already booked for the selected time.")
-                    break  # Exit the loop, no need to retry
-                elif is_successful_booking(response):
-                    log.info("Congratulations! You have successfully booked a gym lesson.")
-                    break  # Exit the loop, booking successful
-
-            log.error("Failed to book a gym lesson. Retrying in 5 minutes...", attempt=attempt_counter + 1)
-            attempt_counter += 1
-            sleep(300)  # Retry after 5 minutes
-        else:
-            log.error("Maximum retry attempts reached. Unable to book a gym lesson.")
+        log.error("Failed to book a gym lesson.")
         
-        logout(logout_url, session_id, log)
+        logout(session_id, log)
     else:
-        log.warning("No schedule found for 7 am on the specified date.")
+        log_message = 'No schedule found for 7 am on the specified date.'
+        log.warning(log_message)
 
 if __name__ == "__main__":
     main()
